9d9039e5a56f6aeadeef65e566a140aab27d0681
ssannssarr <ssantanusarkar8@gmail.com>
2026-05-26 23:24:42 +0530

    practice: update kiosk3

--- kiosk.py
+++ kiosk.py
@@ -1,56 +1,65 @@
-import pandas as pd
+
 
 # LIBRARY      
 library_books = {
     "It Ends With Us":{
+        "name":"It Ends With Us",
         "Availability": True,
         "price":99.5,
         "Author":"Collen Hoover",
         "Stock": 15,
     },
     "1984":{
+        "name":"1984",
         "Availability": False,
         "price":80.50,
         "Author":"Collen Hoover",
         "Stock": 0,
     },
     "One Indian Girl": {
+        "name":"One Indian Girl",
         "Availability": True,
         "price":99.5,
         "Author":"Collen Hoover",
         "Stock": 15,
     },
     "Atomic Habit": {
+        "name":"Atomic Habit",
         "Availability": True,
         "price":60.75,
         "Author":" James Clear",
         "Stock": 15,
     },
     "The Alchemist": {
+        "name":"The Alchemist",
         "Availability": False,
         "price":99.5,
         "Author":"Paulo Coelho",
         "Stock": 0,
     },
     "The Subtle Art of Not Giving a F*ck": {
+        "name":"The Subtle Art of Not Giving a F*ck",
         "Availability": False,
         "price":70.00,
         "Author":"Mark Manson",
         "Stock": 0,
     },
     "The Power of Now": {
+        "name":"The Power of Now",
         "Availability": True,
         "price":100.00,
         "Author":"Eckhart Tolle",
         "Stock": 15,
     },
     "The 5 AM Club": {
+        "name":"The 5 AM Club",
         "Availability": False,
         "price":500.00,
         "Author":"Robin Sharma",
         "Stock": 0,
     },
     "The Great Gatsby": {
+        "name":"The Great Gatsby",
         "Availability": True,
         "price":85.00,
         "Author":"F. Scott Fitzgerald",
@@ -61,73 +70,101 @@ library_books = {
 
 # TEXT SECTION
 HELP_TEXT = """
- type "/list" to list all available books in our library 
- type "/search" to search a specific book info
- type "/exit" to exit our library
- type "/help" to open this menu
+    Available Commands:
+    /list - List all available books in the library 
+    /srch - Search for a book in the library
+    /buy - Buy a book from the library
+    /exit - Exit the library
+    /help - Show this help message
+
 """
 WELCOME_TEXT = """
- Welcome to SannS library!!^_^
- type "/list" to list all available books in our library 
- type "/search" to search a specific book info
- type "/exit" to exit our library
- type "/help" to open command menu
- Have good reading!! yay          
+    Welcome to SannS library!!^_^
+    We have a wide range of books for you to choose from.        
+    For any query email us @sannslib@gmail.com
+    Type "/help" to see the available commands
+
 """
-BYE_TEXT = "Goodbye!!\n Have a good day!"
+BYE_TEXT = "Goodbye!!\nHave a good day!"
 
-
-
+# The Info Func
+def info(book_name):
+    for book in library_books:
+      if book.lower() == book_name:
+        print(f"""\n
+NAME : {library_books.get(book).get("name")}
+AVAILABILITY : {library_books.get(book).get("Availability")}
+PRICE : {library_books.get(book).get("price")}$
+AUTHOR : {library_books.get(book).get("Author")}
+STOCK : {library_books.get(book).get("Stock")}
+""")
+    else:
+        print(f"""\n
+              The book {book_name} is not available in our library 
+              To see the the available books use Cmd:'/list'""")  
+      
 # Searching Function
 def srch():
-     book_name = input("Enter Book Name:")
-     if book_name in library_books:
-        print(pd.Series(library_books.get(book_name)))
-     else:
-        print(f"The book {book_name} is not available in our library \nTo see the the available books use Cmd:'/list'")       
+     print("\nEnter the book Name you want to query:")
+     book_name = input("[search]>>").strip().lower()
+     info(book_name=book_name)    
+
 # List All Available Books
-def list_books():
-    for book in library_books:
+def list_a():
+     print("\nThese Are The Available Books In Our Library:\n")
+     for book in library_books:
         if library_books[book].get("Availability"):
             print(book)
+
+# List All Unvailable currently
+def list_un():
+    print("\nThese Are The Unvailable Books In Our Library:\n")
+    for book in library_books:
+        if library_books[book].get("Availability") == False:
+            print(book)
+     
 # Buy function 
 def buy():
-    print(f"\nWhich book will you buy")
-    buy_book = input("[buy]>> ")
-    if buy_book in library_books:
-        if library_books.get(buy_book).get("Availability"):
-            print(f"""
-The book {buy_book} is available. The info is listed below. 
-{pd.Series(library_books.get(buy_book))}
+    print(f"\nWhich book will you buy?")
+    buy_book = input("[buy]>> ").strip().lower()
+    for book in library_books:
+     if book.lower() == buy_book:
+        if library_books.get(book).get("Availability"):
+            print(f"""\n
+The book {book} is available. The info is listed below. 
+{info(book_name=buy_book)}
 Will You Buy?(y/n)""" )
-            buy = input("[buy]>> ")
+            buy = input("[buy]>> ").strip().lower()
             if buy == "y":
-                library_books[buy_book]["Stock"] -= 1
-                print(f"""
-The book {buy_book} of price {library_books.get(buy_book).get("price")}$ is out for delivery.^_^
+                library_books[book]["Stock"] -= 1
+                print(f"""\n
+The book {book} of price {library_books.get(book).get("price")}$ is out for delivery.^_^
 HAPPY READING
 For any query email us @sannslib@gmail.com""")
             else:
                 print("Whenever you change your mind let us know - SannS Library ")
         else:
             print(f"""
-The book {buy_book} is currently unavailable
+The book {book} is currently unavailable
 If you want you can pre-book it
 For book details use "/info"
 """      )
-            unavai = input("[pre-book]>>")
+            unavai = input("[pre-book]>>").strip().lower()
             if unavai == "/info":
-                print(pd.Series(library_books.get(buy_book)))
+                info(book_name=buy_book)
                 print("Will you Pre-Book:(y/n)")
-                prbk = input("[pre-book]>>").lower()
+                prbk = input("[pre-book]>>").strip().lower()
                 if prbk == "y":
                     print("We will inform the sec the book appears to us")
                 else:
                     print("Your wish!!")
             else:
                 print("You can see our other books by cmd ='/list' ")
-    else: 
-        print(f"The book {buy_book} isnt available in our library or you might havr a typo (T.T)")
+     else: 
+        print(f"The book {buy_book} isnt available in our library or you might havr a typo (T.T)")             
+
+
+
 
 
 # Main Process 
@@ -135,18 +172,21 @@ print(WELCOME_TEXT)
 user_input = ""
 try:
     while user_input != "/exit":
-        user_input = input(">>")
-        if user_input == "/list":
-             print("This books are available in our library")
-             list_books()
-        elif user_input == "/search":
+        user_input = input(">>").strip().lower()
+        if user_input == "/list -a":
+            list_a()
+        elif user_input == "/list -un":
+            list_un()
+        elif user_input == "/srch":
             srch()
         elif user_input == "/help":
-            print(HELP_TEXT)
+            print("\n"+HELP_TEXT)
         elif user_input == "/buy":
             buy()
         elif user_input != "/exit":
-             print("The command you typed didnt support.T_T")
+             print("\nThe command you typed didnt support.T_T")
     print(BYE_TEXT)
 except KeyboardInterrupt:
-     print("\n",BYE_TEXT)
+     print("\n"+BYE_TEXT)    
+             
+       
